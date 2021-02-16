d8fc6666c8b299992926fcfd980301ba58789d4d
IngTian <tzy20001018@gmail.com>
2021-02-16 19:06:42 +0800

    Initial Commit

---
+++ RepairShopManagementSystem-Backend/src/main/java/ca/mcgill/ecse321/repairshopmanagementsystem/RepairShopManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.repairshopmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairShopManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairShopManagementSystemApplication.class, args);
+    }
+
+}
