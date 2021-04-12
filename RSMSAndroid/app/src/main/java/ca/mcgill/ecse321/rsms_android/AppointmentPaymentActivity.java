cb5b495a08f7c7c86ab18c93af51b1a386e60469
Byron Chen <yifei.chen@mail.mcgill.ca>
2021-04-12 11:25:31 +0800

    upd

---
+++ RSMSAndroid/app/src/main/java/ca/mcgill/ecse321/rsms_android/AppointmentPaymentActivity.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.rsms_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appointment_payment);
+
+
+    }
+}
--- RSMSAndroid/app/src/main/java/ca/mcgill/ecse321/rsms_android/AppointmentViewAndPayActivity.java
+++ RSMSAndroid/app/src/main/java/ca/mcgill/ecse321/rsms_android/AppointmentViewAndPayActivity.java
@@ -3,8 +3,11 @@ package ca.mcgill.ecse321.rsms_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class AppointmentViewAndPayActivity extends AppCompatActivity {
@@ -32,5 +35,14 @@ public class AppointmentViewAndPayActivity extends AppCompatActivity {
         AppointmentAdapter appointmentAdapter = new AppointmentAdapter(this, appointments,
                 plateNos, times, prices, statuses);
         myListView.setAdapter(appointmentAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent showPaymentActivity = new Intent(getApplicationContext(), AppointmentPaymentActivity.class);
+//                showPaymentActivity.putExtra("ca.mcgill.ecse321.rsms_android.APP_INDEX", position);
+                startActivity(showPaymentActivity);
+            }
+        });
     }
 }
