55dcbe67b8fea4f7b3566ea3b6ccd7d0c3d221d9
Byron Chen <yifei.chen@mail.mcgill.ca>
2021-04-12 11:58:53 +0800

    upd

--- RSMSAndroid/app/src/main/java/ca/mcgill/ecse321/rsms_android/AppointmentPaymentActivity.java
+++ RSMSAndroid/app/src/main/java/ca/mcgill/ecse321/rsms_android/AppointmentPaymentActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AppointmentPaymentActivity extends AppCompatActivity {
 
@@ -12,11 +13,14 @@ public class AppointmentPaymentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String status = intent.getStringExtra("ca.mcgill.ecse321.rsms_android.STATUS");
+        String price  = intent.getStringExtra("ca.mcgill.ecse321.rsms_android.PRICE");
 
         if (status != "paid"){
-            setContentView(R.layout.appointment_payment_paid);
-        }else{
             setContentView(R.layout.appointment_payment);
+            TextView priceForPayTextView = (TextView) findViewById(R.id.priceForPayTextView);
+            priceForPayTextView.setText(price);
+        }else{
+            setContentView(R.layout.appointment_payment_paid);
         }
     }
 }
